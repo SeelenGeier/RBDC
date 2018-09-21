16538d4d3dfee0b6f269fb6b975d5e1e648fe03e
Philipp Gemind <p.gemind@reply.de>
2018-09-21 12:08:12 +0200

    changed format of repositories to be separated into their own repositories

---
+++ src/js/scene/result.js
@@ -0,0 +1,16 @@
+class resultScene extends Phaser.Scene {
+
+    constructor() {
+        super({key: 'result'});
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        // save new current scene in saveObject
+        saveObject.profiles[saveObject.currentProfile].scene = 'result';
+        saveData();
+    }
+}
