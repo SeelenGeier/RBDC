c1d6ff39395b1f6cb5a2ad7e101d0cf29414a572
SeelenGeier <seelengeier@web.de>
2019-01-07 15:04:07 +0100

    add filter for music and background noises

--- src/js/helper/audio.js
+++ src/js/helper/audio.js
@@ -1,8 +1,8 @@
 let sounds = {};
 
 function playSound(id) {
-    // only play sound
-    if(saveObject.profiles[saveObject.currentProfile].sound) {
+    // only play sounds/music if active
+    if((!config.audio[id].isBackground && saveObject.profiles[saveObject.currentProfile].sound) || (config.audio[id].isBackground && saveObject.profiles[saveObject.currentProfile].music)) {
         if(!sounds.hasOwnProperty(id)) {
             sounds[id] = game.sound.add(id);
         }
