a1b131bab161a6ae3958c5c32301d8cc6c0709ad
Philipp Gemind <p.gemind@reply.de>
2019-01-16 14:54:32 +0100

    retry scroll navigation hiding

--- src/js/game.js
+++ src/js/game.js
@@ -26,9 +26,10 @@ if(fullscreen) {
     // resize game to fit the current window
     gameConfig.width = window.innerWidth;
     gameConfig.height = window.innerHeight;
-    
-    // try to hide mobile navigation bar
-    document.location.href = "#";
+
+    // hide scroll navigation bar on mobile devices
+    /mobile/i.test(navigator.userAgent) && setTimeout(function
+        () {   window.scrollTo(0, 1); }, 1000);
 }else {
     // fix position of game in center of the screen
     canvas.style.maxWidth = gameConfig.width + 'px';
